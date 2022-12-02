c55db705ea24be94fabdf0fa989f58ae58916ea4
Daniel Welsh <danjwelsh24@gmail.com>
2022-12-02 12:13:21 +0000

    refactoring solution

--- src/2022/AdventOfCode/Day2/Day2.fs
+++ src/2022/AdventOfCode/Day2/Day2.fs
@@ -3,57 +3,44 @@ namespace AdventOfCode
 module Day2 =
     let inputPath = "./Day2/input.txt"
 
-    let scores =
-        [ "A", "R"; "B", "P"; "C", "S"; "X", "R"; "Y", "P"; "Z", "S" ] |> Map.ofList
+    let winningPairs = [ "A", "C"; "B", "A"; "C", "B" ] |> Map.ofList
+    let losingPairs = [ "C", "A"; "A", "B"; "B", "C" ] |> Map.ofList
 
-    let itemScore = [ "R", 1; "P", 2; "S", 3; "X", 0; "Y", 3; "Z", 6 ] |> Map.ofList
+    let getLines =
+        Utils.readAllLines (inputPath) |> List.map (fun (x: string) -> x.Split(" "))
 
-    let compareMove l r =
-        let lhs = r
-        let rhs = l
+    type ResultScore =
+        | WIN = 6
+        | DRAW = 3
+        | LOSS = 0
 
-        if lhs = rhs then 3
-        elif lhs = "A" && rhs = "B" then 6
-        elif lhs = "B" && rhs = "C" then 6
-        elif lhs = "C" && rhs = "A" then 6
-        else 0
+    let getResultScore l r =
+        let isWin = (l = "A" && r = "B") || (l = "B" && r = "C") || (l = "C" && r = "A")
+
+        if l = r then ResultScore.DRAW
+        elif isWin then ResultScore.WIN
+        else ResultScore.LOSS
 
     let getScore (move: string[]) =
-        let lu = scores[move[1]]
-        let result = compareMove move[1] move[0]
-        let score = itemScore[lu]
+        let itemScore = [ "A", 1; "B", 2; "C", 3 ] |> Map.ofList
 
-        score + result
-
-    let getWinningPair move =
-        if move = "A" then "B"
-        elif move = "B" then "C"
-        else "A"
-
-    let getLosingPair move =
-        if move = "A" then "C"
-        elif move = "B" then "A"
-        else "B"
+        itemScore[move[1]]
+        + LanguagePrimitives.EnumToValue(getResultScore move[0] move[1])
 
     let getMatchingPair (move: string[]) =
         if move[1] = "Y" then move[0]
-        elif move[1] = "Z" then getWinningPair (move[0])
-        else getLosingPair (move[0])
+        elif move[1] = "Z" then losingPairs[move[0]]
+        else winningPairs[move[0]]
 
-    let part1 =
-        let lines =
-            Utils.readAllLines (inputPath)
-            |> List.map (fun (x: string) -> getScore (x.Split(" ")))
-            |> List.sum
+    let part1: int =
+        let selectionMap = [ "X", "A"; "Y", "B"; "Z", "C" ] |> Map.ofList
 
-        lines
+        getLines
+        |> List.map (fun (x: string[]) -> getScore ([| x[0]; selectionMap[x[1]] |]))
+        |> List.sum
 
     let part2 =
-        let lines =
-            Utils.readAllLines (inputPath)
-            |> List.map (fun (x: string) -> x.Split(" "))
-            |> List.map (fun (x: string[]) -> [| x[0]; getMatchingPair x |])
-            |> List.map (fun (x) -> getScore x)
-            |> List.sum
-
-        lines
+        getLines
+        |> List.map (fun (x: string[]) -> [| x[0]; getMatchingPair x |])
+        |> List.map (fun (x) -> getScore x)
+        |> List.sum
