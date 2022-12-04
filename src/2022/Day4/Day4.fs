e32b43f7b6dafaee2ce1b6ac1ce40c497f4c6123
Daniel Welsh <danjwelsh24@gmail.com>
2022-12-04 10:55:11 +0000

    cleaning up implementation

--- src/2022/Day4/Day4.fs
+++ src/2022/Day4/Day4.fs
@@ -7,33 +7,24 @@ module Day4 =
         let bounds = range.Split("-") |> Array.map int
         [| for i in bounds[0] .. bounds[1] -> i |] |> Set.ofArray
 
+    let getRangesAsSets =
+        inputPath
+        |> Utils.readAllLines
+        |> List.map (fun x -> x.Split(","))
+        |> List.map (fun x -> x |> Array.map rangeToSet)
+        |> List.map (fun x -> (x[0], x[1]))
+
     let isSuperSet (tuple: Set<int> * Set<int>) =
         let (set1, set2) = tuple
-
-        if Set.isSubset set1 set2 || Set.isSubset set2 set1 then
-            1
-        else
-            0
+        Set.isSubset set1 set2 || Set.isSubset set2 set1
 
     let isIntersectingSet (tuple: Set<int> * Set<int>) =
         let (set1, set2) = tuple
+        Set.intersect set1 set2 |> Set.isEmpty
 
-        if Set.intersect set1 set2 |> Set.isEmpty then 0 else 1
+    let sumBools list =
+        list |> List.filter (fun x -> x) |> List.length
 
-    let part1 =
-        inputPath
-        |> Utils.readAllLines
-        |> List.map (fun x -> x.Split(","))
-        |> List.map (fun x -> x |> Array.map rangeToSet)
-        |> List.map (fun x -> (x[0], x[1]))
-        |> List.map isSuperSet
-        |> List.sum
+    let part1 = getRangesAsSets |> List.map isSuperSet |> sumBools
 
-    let part2 =
-        inputPath
-        |> Utils.readAllLines
-        |> List.map (fun x -> x.Split(","))
-        |> List.map (fun x -> x |> Array.map rangeToSet)
-        |> List.map (fun x -> (x[0], x[1]))
-        |> List.map isIntersectingSet
-        |> List.sum
+    let part2 = getRangesAsSets |> List.map isIntersectingSet |> sumBools
