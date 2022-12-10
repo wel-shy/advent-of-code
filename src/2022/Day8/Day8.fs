085f8f2549fcabc32f0d6505a1e8871f24d16b3f
Daniel Welsh <danjwelsh24@gmail.com>
2022-12-10 17:04:15 +0000

    solving part 1

--- src/2022/Day8/Day8.fs
+++ src/2022/Day8/Day8.fs
@@ -2,7 +2,7 @@ namespace AdventOfCode
 
 module Day8 =
 
-    let inputPath = "./Day8/test.txt"
+    let inputPath = "./Day8/input.txt"
     let visibilityMap = Map.empty<string, int>
 
     let getExteriorCount (list: List<int[]>) =
@@ -26,45 +26,85 @@ module Day8 =
         let solver = isTallerThanPrevious row
         row |> Array.mapi (fun x _y -> solver x)
 
-    let getAColumn (c: int) (A: List<int[]>) =
+    let getAColumn (c: int) (A: List<List<int>>) =
         let column =
             A
-            |> List.fold (fun (acc: List<int>) (cur: int[]) -> List.append acc [ cur[c] ]) List.empty<int>
+            |> List.fold (fun (acc: List<int>) (cur: List<int>) -> List.append acc [ cur[c] ]) List.empty<int>
 
         column
 
-    let rotate (A: List<int[]>) =
-        seq {
-            for i in 0 .. A[0].Length - 1 do
-                yield getAColumn (i) A
-        }
-        |> (List.ofSeq >> List.map Array.ofList)
+    // let rotate (A: List<int[]>) =
+    //     seq {
+    //         for i in 0 .. A[0].Length - 1 do
+    //             yield getAColumn (i) A
+    //     }
+    //     |> (List.ofSeq >> List.map Array.ofList)
 
     let rotateBack2DArray A =
         let mutable map = List.empty<List<int>>
 
         map
 
+    let isTallest (list: List<int>) tree = tree > (list |> List.max)
+
+    let getFromLeft (list: List<int>) (index: int) = list[0 .. (index - 1)]
+
+    let getFromRight (list: List<int>) (index: int) =
+        list[index + 1 .. list.Length - 1] |> List.rev
+
+    let getFromTopWithTrees trees x y =
+        let column = getAColumn x trees
+        column[0 .. (y - 1)]
+
+    let getFromBottonWithTrees trees x y =
+        let column = getAColumn x trees
+        column[(y + 1) .. (column.Length - 1)]
+
+    let isExterior (map: List<List<int>>) x y =
+        if y = 0 then true
+        elif x = 0 then true
+        elif y = map.Length - 1 then true
+        elif x = map[y].Length - 1 then true
+        else false
+
+
+    let solve (map: List<List<int>>) x y =
+        if isExterior map x y then
+            true
+        else
+            let row = map[y]
+            // let column = getAColumn x map
+
+            let t = map[y][x]
+
+
+            let left = getFromLeft row x
+            let right = getFromRight row x
+            let top = getFromTopWithTrees map x y
+            let bottom = getFromBottonWithTrees map x y
+
+            let toCheck =
+                [| left; right; top; bottom |]
+                |> List.ofArray
+                |> List.map (fun x -> x |> List.max)
+                |> List.filter (fun tree -> t > tree)
+                |> List.length
+
+            // let tree = map[x][y]
+
+            toCheck > 0
+
 
     let part1 =
         let trees =
             inputPath
             |> Utils.readAllLines
-            |> List.map (fun x -> x |> Seq.toArray |> Array.map (fun y -> int y - int '0'))
+            |> List.map (fun x -> x |> Seq.toList |> List.map (fun y -> int y - int '0'))
 
-        let extCount = trees |> getExteriorCount
-        let fromLeft = trees |> List.map mapRow
-
-        let fromRight =
-            trees |> List.map (fun row -> mapRow (row |> Array.rev)) |> List.map Array.rev
-
-        let fromTop = trees |> rotate |> List.ofSeq |> List.map (fun row -> mapRow (row))
-
-        let fromBotton =
+        let ans =
             trees
-            |> rotate
-            |> List.map Array.rev
-            |> List.map (fun row -> mapRow (row))
-            |> List.map Array.rev
+            |> List.mapi (fun y list -> list |> List.mapi (fun x _ -> solve trees x y))
 
-        fromTop
+        ans
+        |> List.map (fun x -> x |> List.filter (fun isVisible -> isVisible) |> List.length)
+        |> List.sum
