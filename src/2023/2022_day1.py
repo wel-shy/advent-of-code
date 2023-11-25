4b0ddf33b99b23fb35f93e518f4de58ad4c9430c
Daniel Welsh <danjwelsh24@gmail.com>
2023-11-25 12:42:12 +0000

    cleaning up

--- src/2023/2022_day1.py
+++ src/2023/2022_day1.py
@@ -1,14 +1,16 @@
 import lib
 import functools
 
-def reduce_calories(calories, current):
+INPUT_FILE = "./inputs/day_1_test.txt"
+
+def process_calorie(calories, current):
     if current == "\n":
         return calories + [0]
     else:
         return calories[:-1] + [calories[len(calories) - 1] + int(current)]
 
 def parse_to_calories(lines):
-    calories = functools.reduce(lambda acc, cur: reduce_calories(acc, cur), lines, [0])
+    calories = functools.reduce(lambda acc, cur: process_calorie(acc, cur), lines, [0])
     
     return sorted(calories, reverse=True)
 
@@ -19,11 +21,9 @@ def part_2(lines):
     return sum(parse_to_calories(lines)[0:3])
 
 def __main__():
-    lines = lib.read_file("./test.txt")
-    answer_p1 = part_1(lines)
-    answer_p2 = part_2(lines)
+    lines = lib.read_file(INPUT_FILE)
 
-    print(answer_p1)
-    print(answer_p2)
+    print(f"Part 1: {part_1(lines)}")
+    print(f"Part 2: {part_2(lines)}")
 
 __main__()
