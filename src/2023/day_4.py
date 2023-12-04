abeba8e0f511e76c83e19ba6eaccda8e20c62e29
Daniel Welsh <danjwelsh24@gmail.com>
2023-12-04 11:27:47 +0000

    solving day_4 refactoring part 2

--- src/2023/day_4.py
+++ src/2023/day_4.py
@@ -2,6 +2,7 @@ import lib
 import functools
 
 INPUT_FILE = "./inputs/day_4_input.txt"
+lookup = {}
 
 def get_score(score, card):
   card_score = card[0]
@@ -32,7 +33,6 @@ def get_next_cards(cards, card, lines):
   return cards + next_cards
 
 def part_2(lines):
-  card_lookup = {}
   total_cards = len(lines)
   
   cards_to_process = lines
@@ -49,6 +49,74 @@ def part_2(lines):
     
   return total_cards
 
+def get_card_id(line):
+  return int(line.split(": ")[0].strip().replace("Card", "").strip())
+
+def part_2_recursive(lines):
+  # lookup = {}
+  total_cards = len(lines)
+  
+  for line in lines:
+    # print(line, lookup)
+    card_id = get_card_id(line)
+    
+    
+    # get_total_child_cards([line], lines, lookup)
+    child_cards = get_total_child_cards(line, lines)
+    print(card_id, f"Child cards: {child_cards}")
+      
+    total_cards += child_cards
+    
+  # num_cards = get_total_child_cards([lines[0]], lines)
+  # cards = list(map(parse_line, lines))
+  # cards = list(map(lambda card: (card[0].intersection(card[1]), card[2]), cards))
+  # cards = list(map(lambda card: (len(card[0]), card[1]), cards))
+  
+  return total_cards
+
+def get_card_winning_numbers(card):
+  c = parse_line(card)
+  card_id = get_card_id(card)
+  # if card_id in lookup:
+  #   return lookup[card_id]
+  
+  winning_numbers = len(c[0].intersection(c[1]))
+  return winning_numbers
+    # lookup[card_id] = winning_numbers
+    # next_cards = next_cards + lines[card_id:card_id + winning_numbers]
+
+def get_total_child_cards(card, lines):
+  # if len(cards) == 0:
+  #   return 1
+  
+  next_cards = []
+  
+  # for card in cards:
+  card_id = get_card_id(card)
+  
+  winning_numbers = get_card_winning_numbers(card)
+  # if not card_id in lookup:
+  #   lookup[card_id] = winning_numbers
+  
+  next_cards = lines[card_id:card_id + winning_numbers]
+  total = 0
+  for card in next_cards:
+    # print(card_id, winning_numbers)
+    total = total + 1 + get_total_child_cards(card, lines)
+    
+    # if card_id in lookup:
+    #   next_cards = next_cards + lines[card_id:card_id + lookup[card_id]]
+    # else:
+    #   winning_numbers = len(c[0].intersection(c[1]))
+    #   lookup[card_id] = winning_numbers
+    #   next_cards = next_cards + lines[card_id:card_id + winning_numbers]
+    
+    # if len(next_cards) == 0:
+    #   return 1
+  
+  return total
+  
+
 def part_1(lines):
   cards = list(map(parse_line, lines))
   cards = list(map(lambda card: (card[0].intersection(card[1]), card[2]), cards))
@@ -61,4 +129,4 @@ if __name__ == '__main__':
   lines = list(map(lambda l: l.replace("\n", ""), lines))
   
   print(f'Part 1: {part_1(lines)}')   
-  print(f'Part 2: {part_2(lines)}')
+  print(f'Part 2: {part_2_recursive(lines)}')
